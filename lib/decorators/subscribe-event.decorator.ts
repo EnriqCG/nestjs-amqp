fae13a703d0bf82eb430496a7c1c58e3b05561c9
Enrique Carpintero <e.carpintero@icloud.com>
2021-01-04 18:41:26 +0100

    Simpler Consume decorator
    - Implements ConsumerOptions interface
    - Decorator parameters allow both a string and consumerOptions

--- lib/decorators/subscribe-event.decorator.ts
+++ lib/decorators/subscribe-event.decorator.ts
@@ -3,17 +3,12 @@ import { Options } from 'amqplib'
 import { EVENT_METADATA } from '../amqp.constants'
 import { AMQPMetadataConfiguration } from '../amqp.interface'
 
-// TODO: Consumer Options is not being passed here
-export const Consume = (queueName: string, consumerOptions?: Options.Consume): MethodDecorator => {
-  return (target: object, propertyKey: string | symbol, descriptor: PropertyDescriptor) => {
-    SetMetadata<string, AMQPMetadataConfiguration>(
-      EVENT_METADATA,
-      {
-        queueName,
-        target: target.constructor.name,
-        methodName: propertyKey,
-        callback: descriptor.value
-      }
-    )(target, propertyKey, descriptor)
-  }
+interface ConsumerOptions extends Partial<Options.Consume> {
+  queueName: string
+}
+
+export const Consume = (queueNameOrOptions: string | ConsumerOptions): MethodDecorator => {
+  const options = typeof queueNameOrOptions === 'string' ? { queueName: queueNameOrOptions } : queueNameOrOptions
+
+  return SetMetadata(EVENT_METADATA, options)
 }
